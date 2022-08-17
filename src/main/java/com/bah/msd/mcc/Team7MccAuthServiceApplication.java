4a12f05df29e9c55c95e4c5ddcfb26476157bd55
tgtom23 <tgtom@vt.edu>
2022-08-17 12:05:39 -0400

    Intial Repo

---
+++ src/main/java/com/bah/msd/mcc/Team7MccAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bah.msd.mcc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team7MccAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team7MccAuthServiceApplication.class, args);
+	}
+
+}
